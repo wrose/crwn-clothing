86d00083921b06ecbbf96ca785f8ccadf79227a9
William Rose <will@williamro.se>
2020-04-06 15:04:35 -0400

    broken homepage into smaller components, finished styling menu-items

---
+++ src/pages/homepage/homepage.component.tsx
@@ -0,0 +1,13 @@
+// @ts-ignore
+import React from "react";
+
+import './homepage.styles.scss';
+import Directory from "../../components/directory/directory.component";
+
+const HomePage = (): JSX.Element => (
+  <div className='homepage'>
+    <Directory/>
+  </div>
+);
+
+export default HomePage;
